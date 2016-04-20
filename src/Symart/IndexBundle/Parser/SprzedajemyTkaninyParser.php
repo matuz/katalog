1d1c600580d5395cfdfa4780616e4f854e1a2081
Jacek Martuzalski <martuzalski@gmail.com>
2016-04-20 17:34:55 +0200

    Sprzedajemy tkaniny

---
+++ src/Symart/IndexBundle/Parser/SprzedajemyTkaninyParser.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: symart
+ * Date: 17.04.16
+ * Time: 10:37
+ */
+
+namespace Symart\IndexBundle\Parser;
+
+
+class SprzedajemyTkaninyParser
+{
+
+}
---
+++ src/Symart/IndexBundle/Tests/Parser/SprzedajemyTkaninyParserTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: symart
+ * Date: 17.04.16
+ * Time: 10:29
+ */
+
+namespace Symart\IndexBundle\Tests\Parser;
+
+
+class SprzedajemyTkaninyParserTest
+{
+
+}
